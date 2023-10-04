e802ca2844c8098c3d2537180b4e96ccee57bdb1
yuhyelim <hhllsummer07@gmail.com>
2023-10-04 11:52:36 +0900

    231004 hye

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import BaseLayout from "./components/layout/BaseLayout";
 import Home from "./components/Home";
 import InfoList from "./components/info/InfoList";
 import InfoView from "./components/info/InfoView";
-import EnterList from "./components/enter/EnterList";
 import Mypage from "./components/mypage/Mypage";
 import Use_Login from "./components/users/Use_Login";
 import Use_Join from "./components/users/Use_Join";
@@ -25,7 +24,6 @@ function App() {
           <Route path="join" element={<Use_Join />} />
           <Route path="info/:currentPage" element={<InfoList />} />
           <Route path="info/view/:infoSeq" element={<InfoView />} />
-          <Route path="enter" element={<EnterList />} />
           <Route path="mypage" element={<Mypage />} />
         </Route>
       </Routes>
