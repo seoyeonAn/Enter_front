bd7add0caddb95cd76ce5edb22c7462488fe59a0
tion <seoyson31@gmail.com>
2023-10-15 15:59:34 +0900

    1015

--- src/components/users/Logout.js
+++ src/components/users/Logout.js
@@ -1,12 +1,19 @@
 import React, { useEffect } from "react";
+import { persist } from "../../toolkit/store";
 
 const Logout = () => {
+  
+  // const  onCheckLogout = async()=>{
+  //   await persist.purse();
+  // }
   useEffect(() => {
     localStorage.removeItem("Authorization");
     localStorage.removeItem("email");
     localStorage.removeItem("name");
     localStorage.removeItem("isLogin");
     localStorage.clear();
+  //  onCheckLogout();
+    
     window.location.replace("/");
   });
 };
--- src/toolkit/store.js
+++ src/toolkit/store.js
@@ -1,9 +1,20 @@
+
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import infoSlice from "./reducers/info_reducer";
 import { slice } from "./reducers/mypage_reducer";
 import algoSlice from "./reducers/algorithm_reducer";
-import { persistReducer } from "redux-persist";
+import { persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+
+//import persistStore from "redux-persist/es/persistStore";
+
+import logger from 'redux-logger'
+
 
 // const store = configureStore({
 //   reducer: {
@@ -32,8 +43,20 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
+
+
 const store = configureStore({
   reducer: persistedReducer,
-});
+  //middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            }
+        })
+  
+ });
 
-export default store;
+
+
+export default store; 
